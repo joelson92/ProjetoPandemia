fe356f9fb480aeed465aa0f8fb03de8ff7c18bcf
joelson92 <joelsonveras5@gmail.com>
2022-02-16 10:06:08 -0300

    branch update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Joelson Veras
+          Teste
         </p>
         <a
           className="App-link"
